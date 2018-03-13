21bd0bc6cf682972d80043925607a7e26b74087a
Group 13 <g13capstone@gmail.com>
2018-03-12 21:44:15 -0500

    Moved Sql scripts to own folder, renamed update_stalls2 to update_stalls

--- Web Server - Database - Web App/update_stalls.php
+++ Web Server - Database - Web App/update_stalls.php
@@ -1,35 +1,51 @@
 <?php
+//update stalls script with hash parameter
 
 $lot_id = $_POST['lot_id'];
 $stall_id = $_POST['stall_id'];
 $new_status = $_POST['new_status'];
-
-$mysqli = new mysqli();
-
-// Connect to database
-require 'db_login.php';
+$hash = $_POST['hash'];
 
 
-// Build query to insert product
-$query = "UPDATE ParkingStalls SET status=". $new_status ." WHERE lot_id='". $lot_id . "' AND stall_id='" . $stall_id . "';";
-//printf("%s",$query);
-//Run query
-$result = $mysqli->query($query,MYSQLI_STORE_RESULT);
-$errNum = $mysqli->errno;
+$salt = 'c75aa354fcf34894b997bcf367caae32';
 
 
-if ($errNum) {
-    printf("ERROR: Problem with update");
+$input = $lot_id . $stall_id . $new_status . $salt;
 
+$calc_hash = hash('sha256', $input);
+
+if ($calc_hash == $hash) {
+    // Connect to database
+    $mysqli = new mysqli();
+    
+    require 'db_login.php';
+    
+    // Build query to insert product
+    $query = "UPDATE ParkingStalls SET status=". $new_status ." WHERE lot_id='". $lot_id . "' AND stall_id='" . $stall_id . "';";
+    //printf("%s",$query);
+    //Run query
+    $result = $mysqli->query($query,MYSQLI_STORE_RESULT);
+    $errNum = $mysqli->errno;
+    
+    
+    if ($errNum) {
+    	printf("Error in query: %s",$mysqli->error);
+    
+    }
+    else {
+    
+    	printf("Stall status has been updated");
+    
+    }
+    
+    
+    // Close the database connection
+    $mysqli->close();
 }
 else {
-
-	printf("Stall has been updated");
-
+    printf("ERROR: Authentication Failed");
 }
 
 
-// Close the database connection
-$mysqli->close();
 
 ?>
